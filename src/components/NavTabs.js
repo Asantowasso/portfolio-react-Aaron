f0bb281c7647eb8060d6a2ad6607705d7752653e
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-01-10 21:06:02 -0500

    added files and folders

---
+++ src/components/NavTabs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// The user will be able to navigate the portfolio using navigation tabs at the top of the page
+
+function NavTabs({currentPage, handlePageChange }){
+  return(
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+      <a href ="#home" onClick={() => handlePageChange('Home')}
+
+      className = {currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+      >
+        Home
+        </a>
+      </li>
+    </ul>
+
+  )
+
+}
