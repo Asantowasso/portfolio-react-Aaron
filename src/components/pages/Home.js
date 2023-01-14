ee9e206a00eb2185f945e7f5336c9b5592236f17
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-01-14 18:00:41 -0500

    added styling to home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import '../styles/Home.css'
 //This will serve as the "home" page it will be what the visitor first sees when they open my site
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Home</h1>
-      <p>Sample text, Sample text</p>
+      <p>My name is Aaron Santowasso. I am a recent graduate of the coding bootcamp provided through the University of Richmond. </p>
     </div>
   );
 }
