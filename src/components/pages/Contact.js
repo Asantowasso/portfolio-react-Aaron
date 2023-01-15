571c4e2179b6ad74e66a2f17025a1a112e41f57e
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-01-15 13:28:56 -0500

    added styling and form for custom messages

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,7 +6,30 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact</h1>
-      <p>Here are the items on my Resume</p>
+      <form className="form">
+        <h3>Name</h3>
+        <input
+        name="Name"
+        placeholder="Your Name"
+        >
+        
+        </input>
+        <h3>Email</h3>
+        <input
+        name="email"
+        placeholder="Your email address"
+        >
+
+        </input>
+        <h3>Message</h3>
+        <input
+        name="message"
+        placeholder="What do you want to say?"
+        >
+        
+        </input>
+
+      </form>
 
     </div>
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className="home">
       <h1>Home</h1>
-      <p>My name is Aaron Santowasso. I am a recent graduate of the coding bootcamp provided through the University of Richmond. </p>
+      <h2>About Aaron</h2>
+      <p> My name is Aaron Santowasso. I am a recent graduate of the coding bootcamp provided through the University of Richmond. I am interested in pursuing a career in web or software development. </p>
+      <p>This course has not only taught me the skills I will need in order to be a successful employee, it has also given me valuable lessons in time management and working alongside others. On this site you will find a portfolio of my work, my resume and means of contacting me.  </p>
     </div>
   );
 }
