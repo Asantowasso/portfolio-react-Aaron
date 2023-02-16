20cc2f7e29aa2f748178060360c50a5bde6fb575
Asantowasso <79666127+Asantowasso@users.noreply.github.com>
2023-02-15 22:06:14 -0500

    changed images

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/Work.css";
-// import vet from "../assets/vetKP.jpg";
-// import api from "../assets/APIproject.jpg";
-// import coffee from "../assets/coffeeshop.jpg";
-// import typing from "../assets/typing.jpg";
-
+import vet from "../assets/vetKP.jpg";
+import api from "../assets/API.png";
+import coffee from "../assets/coffee.jpg";
+import typing from "../assets/typing.jpg";
+import quiz from "../assets/mapquiz.jpg";
+import table from "../assets/table.jpg";
 // This is the "myWork" page and it will be where my work will be showcased
 
 export default function Work() {
@@ -17,13 +18,13 @@ export default function Work() {
       </p>
       <li class="image">
         <h3>Pet-to-Vet</h3>
-        {/* <img
+        <img
           src={vet}
           class="image__img"
           alt="Vet with a kitten and puppy"
           width="320"
           height="200"
-        /> */}
+        />
         <div class="image__overlay">
           <div class="image__title">Create a profile for your pet</div>
           <p class="image__description"> <a href = "https://github.com/Asantowasso/pet-to-vet">Github</a> <a href = "https://damp-forest-39395.herokuapp.com/login"> Deployed</a></p>
@@ -32,12 +33,12 @@ export default function Work() {
 
       <li>
         <h3>Social Media API</h3>
-        {/* <img
+        <img
           src={api}
           alt="Represents the features of an application to store and manage information about users"
           width="320"
           height="200"
-        /> */}
+        />
         <div class="image__overlay">
           <div class="image__title">Find and manage user profiles</div>
           <p class="image__description"><a href = "https://github.com/Asantowasso/Social-api-Aaron">Github</a> <a href= "https://drive.google.com/file/d/1e66kPc6XpYh-KxASAZ5azrFmF9Rurcrr/view">Demonstration</a></p>
@@ -46,12 +47,12 @@ export default function Work() {
 
       <li>
         <h3>Moonbucks</h3>
-        {/* <img
+        <img
           src={coffee}
           alt="Represents the features of an application to store and manage information about users"
           width="320"
           height="200"
-        /> */}
+        />
         <div class="image__overlay">
           <div class="image__title">
             A mock website featuring a currency coverter
@@ -62,12 +63,12 @@ export default function Work() {
 
       <li>
         <h3>Text editor</h3>
-        {/* <img
+        <img
           src={typing}
           alt="a person typing on a laptop keyboard"
           width="320"
           height="200"
-        /> */}
+        />
         <div class="image__overlay">
           <div class="image__title">
             PWA that can be run in the browser or installed
@@ -75,6 +76,40 @@ export default function Work() {
           <p class="image__description"><a href="https://github.com/Asantowasso/text-editor-Aaron">Github</a> <a href="https://hidden-inlet-47886.herokuapp.com/">Deployed</a></p>
         </div>
       </li>
+
+      <li>
+        <h3>Geography Quiz</h3>
+        <img
+          src={quiz}
+          alt="A compass and map"
+          width="320"
+          height="200"
+        />
+        <div class="image__overlay">
+          <div class="image__title">
+           Test your knowledge of world Geography
+          </div>
+          <p class="image__description"><a href="https://github.com/Asantowasso/text-editor-Aaron">Github</a> <a href="https://hidden-inlet-47886.herokuapp.com/">Deployed</a></p>
+        </div>
+      </li>
+
+      <li>
+        <h3>Gastronauts</h3>
+        <img
+          src={table}
+          alt="a person typing on a laptop keyboard"
+          width="320"
+          height="200"
+        />
+        <div class="image__overlay">
+          <div class="image__title">
+          Create a profile and make a reservation
+          </div>
+          <p class="image__description"><a href="https://github.com/Asantowasso/text-editor-Aaron">Github</a> <a href="https://hidden-inlet-47886.herokuapp.com/">Deployed</a></p>
+        </div>
+      </li>
     </ul>
+
+    
   );
 }
